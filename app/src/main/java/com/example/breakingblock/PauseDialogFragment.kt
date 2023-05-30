2342d4c1b7b94307fad6f41d589144b4c356abc3
kingmingseo <multi1971@mju.ac.kr>
2023-05-30 23:06:29 +0900

    커스텀 다이얼로그 활용 일시정지 기능 구현
    커스텀 다이얼로그를 활용하여 일시정지 기능을 구현
    dialog 내부에 retry continue exit를 통해서 
    메인메뉴로 돌아가능 기능 또한 구현 
    휴대폰 뒤로가기 버튼을 실수로 눌렀을 때 게임이 바로 꺼져버리는 상황을 방지

---
+++ app/src/main/java/com/example/breakingblock/PauseDialogFragment.kt
@@ -0,0 +1,58 @@
+package com.example.breakingblock
+
+import android.graphics.Color
+import android.graphics.drawable.ColorDrawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
+import com.example.breakingblock.databinding.PauseLayoutBinding
+
+class PauseDialogFragment : DialogFragment() {
+    private var _binding: PauseLayoutBinding? = null
+    private val binding get() = _binding!!
+    private var continueClickListener: (() -> Unit)? = null
+    private var exitClickListener: (() -> Unit)? = null
+    private var retryClickListener: (() -> Unit)? = null
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+        _binding = PauseLayoutBinding.inflate(inflater, container, false)
+        val view = binding.root
+        dialog?.setCancelable(false)
+        // 레이아웃 배경을 투명하게 해줌, 필수 아님
+        dialog?.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        binding.retryBtn.setOnClickListener {
+            retryClickListener?.invoke()
+            dismiss()
+
+        }
+        binding.exitBtn.setOnClickListener {
+            exitClickListener?.invoke()
+            dismiss()
+        }
+        binding.continueBtn.setOnClickListener {
+            continueClickListener?.invoke()
+            dismiss()
+        }
+
+        return view
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+    fun setOnContinueClickListener(listener: () -> Unit) {
+        continueClickListener = listener
+    }
+    fun setOnExitClickListener(listener: () -> Unit) {
+        exitClickListener = listener
+    }
+    fun setOnRetryClickListener(listener: () -> Unit) {
+        retryClickListener = listener
+    }
+
+
+}
